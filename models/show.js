8685c0573949fbf2ac1caea71afac69e528bd19a
micahwierenga <wierenga.micah@gmail.com>
2020-05-20 09:06:07 -0600

    Adds data

--- models/show.js
+++ models/show.js
@@ -1,11 +1,15 @@
 const shows = [
-    { id: 1, name: '', cast: [] },
-    { id: 2, name: '', cast: [] },
-    { id: 3, name: '', cast: [] },
-    { id: 4, name: '', cast: [] },
-    { id: 5, name: '', cast: [] },
-    { id: 6, name: '', cast: [] },
-    { id: 7, name: '', cast: [] },
+    { id: 1, name: 'The Office', cast: ['Steve Carell', 'Rainn Wilson', 'John Krasinski'] },
+    { id: 2, name: 'It\s Always Sunny in Philadelphia', cast: ['Rob McElhenney', 'Charlie Day', 'Glenn Howerton'] },
+    { id: 3, name: 'Seinfeld', cast: ['Jerry Seinfeld', 'Julia Louis-Dreyfus', 'Jason Alexander'] },
+    { id: 4, name: 'Parks and Recreation', cast: ['Amy Poehler', 'Nick Offerman'] },
+    { id: 5, name: 'Breaking Bad', cast: ['Bryan Cranston', 'Aaron Paul'] },
+    { id: 6, name: 'Stranger Things', cast: ['Millie Bobby Brown', 'Finn Wolfhard', 'Noah Schnapp'] },
+    { id: 7, name: 'Letterkenny', cast: ['Jared Keeso', 'Michelle Mylett'] },
+    { id: 8, name: 'Shameless', cast: ['Emmy Rosen', 'Ethan Cutkosky', 'William H Macy'] },
+    { id: 9, name: 'BoJack Horseman', cast: ['Will Arnett', 'Amy Sedaris'] },
+    { id: 10, name: 'Brooklyn Nine Nine', cast: ['Andy Samberg', 'Melissa Fumero'] },
+    { id: 11, name: 'How I Met Your Mother', cast: ['Cobie Smulders', 'Neil Patrick Harris'] },
 ];
 
 module.exports = {
